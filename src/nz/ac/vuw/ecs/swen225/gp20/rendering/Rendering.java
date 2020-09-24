3ec166e7342f005698c6290c697335b0a946c79b
malataarno <malataarno@myvuw.ac.nz>
2020-09-25 09:53:49 +1200

    Preparing methods

--- src/nz/ac/vuw/ecs/swen225/gp20/rendering/Rendering.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/rendering/Rendering.java
@@ -1,5 +1,18 @@
 package nz.ac.vuw.ecs.swen225.gp20.rendering;
 
-public class Rendering {
+import java.awt.*;
 
+public class Rendering {
+    Dimension size;
+    Point position;
+
+    public void setSize(Dimension d){size = d;}
+    public void reCenter(Point p){position = p;}
+    public void draw(Graphics g){}
+
+
+    private boolean runLeft(){return false;}
+    private boolean runRight(){return false;}
+    private boolean runUp(){return false;}
+    private boolean runDown(){return false;}
 }
