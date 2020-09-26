69010f844cfd4f6b9c789b351423bc9827c2d4a4
malataarno <malataarno@myvuw.ac.nz>
2020-09-27 03:58:34 +1300

    Animation Test

--- src/nz/ac/vuw/ecs/swen225/gp20/application/Main.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/application/Main.java
@@ -1,15 +1,24 @@
 package nz.ac.vuw.ecs.swen225.gp20.application;
 
+import nz.ac.vuw.ecs.swen225.gp20.rendering.Rendering;
+
 import java.awt.*;
 
 public class Main extends GUI {
+  static Rendering r;
   public static void main(String... args) {
+
     Main game = new Main();
+    r = new Rendering();
+
   }
 
   @Override
   protected void redraw(Graphics g, Dimension d) {
     g.setColor(Color.LIGHT_GRAY);
     g.fillRect(0,0,d.width,d.height);
+    r.testDrawingAnimation(g,null,"Down");
+    redraw();
+
   }
 }
