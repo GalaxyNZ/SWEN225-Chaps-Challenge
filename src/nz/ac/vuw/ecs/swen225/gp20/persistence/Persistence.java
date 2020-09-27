ef10cb21350584ff43bc1e38062b22bff0604fe8
bathgaoliv <bathgaoliv@ecs.vuw.ac.nz>
2020-09-28 00:17:00 +1300

    Reads json file with better formatting.

--- src/nz/ac/vuw/ecs/swen225/gp20/persistence/Persistence.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/persistence/Persistence.java
@@ -11,6 +11,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 
 public class Persistence {
@@ -29,20 +30,39 @@ public class Persistence {
 
       //convert to Gson
       // convert JSON file to map
-      Map<?, ?> map = gson.fromJson(reader, Map.class);
+      Map<String, String> map = gson.fromJson(reader, Map.class);
 
       // print map entries
-      for (Map.Entry<?, ?> entry : map.entrySet()) {
-        //System.out.println(entry.getKey() + "=" + entry.getValue());
-        System.out.println(entry.getValue());
+      for (Map.Entry<String, String> entry : map.entrySet()) {
+
+        String varName = entry.getKey();
+        if(varName.equals("board")){
+          readBoard(entry.getValue());
+        }
+        else System.out.println(entry.getKey() + " = " + entry.getValue());
       }
 
-      // close reader
       reader.close();
 
     } catch (Exception ex) {
       ex.printStackTrace();
     }
 
+  }
+
+  public static void readBoard(String maze){
+    Scanner sc = new Scanner(maze).useDelimiter("\\,");
+    int count = 0;
+
+    while(sc.hasNext()){
+      System.out.print("|");
+      System.out.print(sc.next());
+      count++;
+      if( count == 15){
+        System.out.print("|");
+        System.out.println();
+        count = 0;
+      }
+    }
   }
 }
