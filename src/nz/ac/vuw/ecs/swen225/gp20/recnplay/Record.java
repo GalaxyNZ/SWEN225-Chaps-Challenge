7c479dc0cf8347680099c326b38e76205e05e52c
phampphilip <phamhoan4@myvuw.ac.nz>
2020-09-30 19:35:11 +1300

    Record writes to a json file.

--- src/nz/ac/vuw/ecs/swen225/gp20/recnplay/Record.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/recnplay/Record.java
@@ -1,4 +1,61 @@
 package nz.ac.vuw.ecs.swen225.gp20.recnplay;
 
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+
 public class Record {
+  //private ArrayList<String> moves = new ArrayList<>();
+  public Queue<String> q = new ArrayDeque<>();
+  private Boolean isRecording = false;
+
+  public void record() {
+
+    q.add("a");q.add("b");q.add("c");q.add("d");q.add("e");
+
+
+
+    JSONArray moves = new JSONArray();
+
+    JSONObject file = new JSONObject();
+
+
+    while (!q.isEmpty()){
+      moves.add(q.poll());
+    }
+
+    file.put("xSize", 50);
+    file.put("ySize", 50);
+    file.put("tileInfo", "boop beep boop");
+    file.put("SETGK", 3);
+    file.put("SETBK", 2);
+    file.put("SETYK", 1);
+    file.put("SETRK", 2);
+    file.put("board", "boop beep boop");
+    file.put("moves", moves);
+
+    try (FileWriter reee = new FileWriter("recording.json")) {
+
+      reee.write(file.toJSONString());
+      reee.flush();
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+
+
+  public static void main(String[] args) {
+    Record r = new Record();
+    r.record();
+  }
+
 }
