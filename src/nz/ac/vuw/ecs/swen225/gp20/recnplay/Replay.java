53fce2b491c58b62eeef7eb3a2db4312e70c66d7
phampphilip <phamhoan4@myvuw.ac.nz>
2020-10-20 15:57:42 +1300

    methods

--- src/nz/ac/vuw/ecs/swen225/gp20/recnplay/Replay.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/recnplay/Replay.java
@@ -1,6 +1,7 @@
 package nz.ac.vuw.ecs.swen225.gp20.recnplay;
 
 import nz.ac.vuw.ecs.swen225.gp20.application.Main;
+import nz.ac.vuw.ecs.swen225.gp20.maze.Maze;
 
 public class Replay {
   Main main;
@@ -10,6 +11,18 @@ public class Replay {
   public Replay(Main main){
     this.main = main;
 
+  }
+
+  public Maze loadReplay(){
+
+    return null;
+  }
+
+  public void autoStep(){
+
+  }
+
+  public void iterateStep(){
 
   }
 }
