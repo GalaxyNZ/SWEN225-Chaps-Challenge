01c604284fedb54a7f254e87e1c7a876749df302
malataarno <malataarno@myvuw.ac.nz>
2020-10-23 18:54:32 +1300

    Removed class

--- src/nz/ac/vuw/ecs/swen225/gp20/rendering/ErrorMessage.java
+++
@@ -1,9 +0,0 @@
-package nz.ac.vuw.ecs.swen225.gp20.rendering;
-
-public class ErrorMessage extends Exception{
-    public ErrorMessage(String s){
-        System.out.println(s);
-        System.exit(1);
-    }
-
-}
