a624e3d305007e2b2803d1ce52e4dc61e7eac9b0
catherluke <ljcatherall@gmail.com>
2020-10-23 18:09:45 +1300

    Updated JavaDocs, added a third monkeytest board

--- src/test/nz/ac/vuw/ecs/swen225/gp20/monkey/FixedTests.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp20/monkey/FixedTests.java
@@ -11,11 +11,20 @@ import javax.json.*;
 import java.util.HashMap;
 import java.util.Scanner;
 
+/*
+ * This class represents the fixed testing part of this project.
+ * Here tests are run to ensure all core systems are running properly so the game can be played.
+ *
+ * @author Luke Catherall - catherluke
+ */
+
 public class FixedTests {
 
     /*
+        * Key table for the board toString output
+
         * X = Chap
-        *   = Free Tile
+        * _ = Free Tile
         * # = Wall
         * % = Exit lock item
         * I = Info tile
@@ -35,7 +44,9 @@ public class FixedTests {
 
 
     // BASIC TESTS
-
+    /*
+     * Tests simple board generation
+     */
     @Test
     public void t01_SimpleBoardGeneration() {
         String map =  "5|5|SAMPLE TILE INFO|11|SETBK|0|SETYK|0|SETRK|0|SETGK|0|"
@@ -45,7 +56,7 @@ public class FixedTests {
                 + "_|_|_|_|_|"
                 + "_|_|_|_|_";
 
-        JsonObject test = toJSON(map);
+        JsonObject test = toJSON(map); // Convers map string to a JSON file
 
         String moves = "";
 
@@ -56,14 +67,18 @@ public class FixedTests {
                         "|_|_|_|_|_|\n" +
                         "|_|_|_|_|_|\n";
 
-        Persistence p = new Persistence();
-        Maze maze = p.loadJsonString(test.toString());
+        Persistence p = new Persistence(); // Creates a persistence instance
+        Maze maze = p.loadJsonString(test.toString()); // Loads the map json file
 
-        assertEquals(expected, maze.toString());
-        assertEquals(0, maze.getPlayerChips());
-        assertEquals("[]", maze.getPlayerInv().toString());
+        assertEquals(expected, maze.toString()); // Checks the map is correct
+        assertEquals(0, maze.getPlayerChips()); // Checks the number of chips is correct
+        assertEquals("[]", maze.getPlayerInv().toString()); // Checks the inventory is correct
     }
 
+
+    /*
+     * Tests a more complex board generation with every available tile kind.
+     */
     @Test
     public void t02_ComplexBoardGeneration() {
         String map =  "17|16|SAMPLE TILE INFO|11|SETBK|1|SETYK|1|SETRK|1|SETGK|2|" +
@@ -112,6 +127,10 @@ public class FixedTests {
         assertEquals("[]", maze.getPlayerInv().toString());
     }
 
+
+    /*
+     * Tests simple movement in all directions.
+     */
     @Test
     public void t03_SimpleMovement() {
         String map =  "5|5|SAMPLE TILE INFO|11|SETBK|0|SETYK|0|SETRK|0|SETGK|0|"
@@ -143,6 +162,9 @@ public class FixedTests {
 
     // Restricted movement
 
+    /*
+     * Ensures walls work properly.
+     */
     @Test
     public void t04_Walls() {
         String map =  "5|5|SAMPLE TILE INFO|11|SETBK|1|SETYK|0|SETRK|0|SETGK|0|"
@@ -152,7 +174,7 @@ public class FixedTests {
                 + "_|#|#|#|_|"
                 + "_|_|_|_|_|";
 
-        String[] moves = new String[]{ "w", "w", "w"};
+        String[] moves = new String[]{ "w", "w", "w", "a", "a", "a", "s", "s", "s", "d", "d", "d"};
 
         String expected =
                 "|_|_|_|_|_|\n" +
@@ -172,7 +194,9 @@ public class FixedTests {
         assertEquals("[]", maze.getPlayerInv().toString());
     }
 
-
+    /*
+     * Ensures you cannot walk off the map
+     */
     @Test
     public void t05_MovementOffMap() {
         String map =  "5|5|SAMPLE TILE INFO|11|SETBK|1|SETYK|0|SETRK|0|SETGK|0|"
@@ -204,6 +228,10 @@ public class FixedTests {
     }
 
     // DOORS and KEYS
+
+    /*
+     * Tests that doors can be constructed, opened and remain open
+     */
     @Test
     public void t06_LockedDoors() {
         String map =  "5|5|SAMPLE TILE INFO|11|SETBK|1|SETYK|0|SETRK|0|SETGK|0|"
@@ -235,6 +263,9 @@ public class FixedTests {
     }
 
 
+    /*
+     * Checks that the correct key uis used to open the correct dooe
+     */
     @Test
     public void t07_NoKeyUnlockingDoor() {
         String map =  "5|5|SAMPLE TILE INFO|11|SETBK|1|SETYK|0|SETRK|0|SETGK|0|"
@@ -265,6 +296,10 @@ public class FixedTests {
         assertEquals("[b]", maze.getPlayerInv().toString());
     }
 
+
+    /*
+     * Checks doors cannot be unlocked without the key
+     */
     @Test
     public void t08_IncorrectKeyUnlockingDoor() {
         String map =  "5|5|SAMPLE TILE INFO|11|SETBK|1|SETYK|0|SETRK|0|SETGK|0|"
@@ -298,6 +333,9 @@ public class FixedTests {
 
     // ITEM TESTS
 
+    /*
+     * Checks whether treasure can be picked up
+     */
     @Test
     public void t09_PickUpTreasure() {
         String map =  "5|5|SAMPLE TILE INFO|11|SETBK|1|SETYK|0|SETRK|0|SETGK|0|"
@@ -328,6 +366,10 @@ public class FixedTests {
         assertEquals(5, maze.getPlayerChips());
         assertEquals("[]", maze.getPlayerInv().toString());
     }
+
+    /*
+     * Checks whether keys can be picked up
+     */
 
     @Test
     public void t10_PickUpKeys() {
@@ -360,6 +402,9 @@ public class FixedTests {
         assertEquals("[r, g, b, y]", maze.getPlayerInv().toString());
     }
 
+    /*
+     * Checks whether multiple of the same keys can be picked up.
+     */
     @Test
     public void t11_PickUpMultipleKeys() {
         String map =  "5|5|SAMPLE TILE INFO|11|SETBK|1|SETYK|0|SETRK|0|SETGK|0|"
@@ -392,6 +437,9 @@ public class FixedTests {
     }
 
 
+    /*
+     * Checks the info tile can be walked on, and has the correct info.
+     */
     @Test
     public void t12_InfoItem() {
         String map =  "5|5|SAMPLE TILE INFO|11|SETBK|1|SETYK|0|SETRK|0|SETGK|0|"
@@ -426,6 +474,9 @@ public class FixedTests {
     }
 
     // Ending Game
+    /*
+     * Makes sure the game state can be ended. and that the player cannot walk onto it
+     */
     @Test
     public void t13_SimpleGameEnd() {
         String map =  "5|5|SAMPLE TILE INFO|0|SETBK|0|SETYK|0|SETRK|0|SETGK|0|"
@@ -460,6 +511,11 @@ public class FixedTests {
 
     // Break it
 
+    /*
+     * Gives bad inputs and hopes a board cannot be constructed with them
+     *
+     * This will throw an IndexOutOfBoundsException and NullPointerException if it works correctly
+     */
     @Test
     public void t14_badBoardConstruction() {
         String map =  "5|5|SAMPLE TILE INFO|11|SETBK|0|SETYK|0|SETRK|0|SETGK|0|"
@@ -492,6 +548,11 @@ public class FixedTests {
         assertTrue(catchError);
     }
 
+    /*
+     * Gives incorrect sizes in the board data. Claims it is 7x32 but board is only 5x5
+     *
+     * This will throw an IndexOutOfBoundsException if it works correctly
+     */
     @Test
     public void t15_incorrectSize() {
         String map =  "7|32|SAMPLE TILE INFO|11|SETBK|0|SETYK|0|SETRK|0|SETGK|0|"
@@ -526,6 +587,9 @@ public class FixedTests {
     }
 
 
+    /*
+     * Ensures a map cannot be created without chap.
+     */
     @Test
     public void t16_noChap() {
         String map =  "5|5|SAMPLE TILE INFO|11|SETBK|0|SETYK|0|SETRK|0|SETGK|0|"
@@ -551,6 +615,9 @@ public class FixedTests {
 
     }
 
+    /*
+     * Loops through the predefined movements
+     */
     public void gameplayLoop(Maze maze, String[] moves) {
         for (String s : moves) {
             switch (s) {
@@ -570,11 +637,15 @@ public class FixedTests {
         }
     }
 
+
+    /*
+     * turns the old map generation string into the new JSON generation file
+     */
     public static JsonObject toJSON(String map) {
         Scanner scan = new Scanner(map);
         scan.useDelimiter("\\|");
 
-        HashMap<String, Integer> config = new HashMap();
+        HashMap<String, Integer> config = new HashMap<>();
         JsonBuilderFactory factory = Json.createBuilderFactory(config);
 
         JsonObjectBuilder test = factory.createObjectBuilder();
--- src/test/nz/ac/vuw/ecs/swen225/gp20/monkey/MonkeyTest.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp20/monkey/MonkeyTest.java
@@ -9,8 +9,16 @@ import javax.json.JsonObject;
 
 import static test.nz.ac.vuw.ecs.swen225.gp20.monkey.FixedTests.toJSON;
 
+/*
+ * This class represents the Monkey Testing aspect of this project.
+ *
+ * @author Luke Catherall - catherluke
+ */
+
 public class MonkeyTest {
 
+
+    // Default board to use
     String map =  "17|16|SAMPLE TILE INFO|11|SETBK|1|SETYK|1|SETRK|1|SETGK|2|" +
             "_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|" +
             "_|_|_|#|#|#|#|#|_|#|#|#|#|#|_|_|_|" +
@@ -29,17 +37,25 @@ public class MonkeyTest {
             "_|_|_|_|_|#|#|#|#|#|#|#|_|_|_|_|_|" +
             "_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|";
 
+
+    /**
+     * Gives the game a random input until the game is won.
+     */
     @Test
     public void MonkeyTesting() {
         JsonObject test = toJSON(map);
         Persistence p = new Persistence();
-        //Maze maze = p.loadJsonString(test.toString());
 
-        //TODO: make multiple boards and test them all.
-
-        Maze maze = p.newGame();
+        // Test default board
+        Maze maze = p.loadJsonString(test.toString());
         MonkeyTestingLoop(maze);
 
+        // Test level 1
+        maze = p.newGame();
+        MonkeyTestingLoop(maze);
+
+
+        // Test level 2
         maze = p.loadLevelTwo();
         MonkeyTestingLoop(maze);
 
@@ -47,7 +63,7 @@ public class MonkeyTest {
 
     private void MonkeyTestingLoop(Maze maze) {
         int count = 0;
-        while (!maze.levelWonChecker()) {
+        while (!maze.levelWonChecker() && !maze.moveBugs()) {
             int rand = (int) (Math.random() * 4);
             switch (rand) {
                 case 0:
